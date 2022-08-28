8e9b671cec56379eb6ec34cec0b9e113c6f35787
phalmeidal <93348003+phalmeidal@users.noreply.github.com>
2022-08-28 11:51:09 -0300

    Projeto Completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,36 @@
+import app from "./firebase/app.js";
+import { getHellfireClubSubscriptions, subscribeToHellfireClub } from "./firebase/hellfire-club.js";
+
+
 const txtName = document.getElementById("txtName");
 const txtEmail = document.getElementById("txtEmail");
 const txtLevel = document.getElementById("txtLevel");
 const txtCharacter = document.getElementById("txtCharacter");
- 
- 
+
+
 const btnSubscribe = document.getElementById("btnSubscribe");
-btnSubscribe.addEventListener('click', ()=>{
-    const subscription ={
+btnSubscribe.addEventListener('click', async () => {
+    const subscription = {
         name: txtName.value,
-        email:txtEmail.value,
-        level:txtLevel.value,
-        character:txtCharacter.value
+        email: txtEmail.value,
+        level: txtLevel.value,
+        character: txtCharacter.value
     }
-   
+
+    const subscriptionId = await subscribeToHellfireClub(subscription)
+
+    txtName.value = ''
+    txtEmail.value = ''
+    txtLevel.value = ''
+    txtCharacter.value = ''
+
+    alert(`Inscrito com sucesso: ${subscriptionId}`)
+
 })
+
+async function loadData(){
+    const subscriptions = await getHellfireClubSubscriptions()
+    console.log(subscriptions)
+}
+loadData()
 
